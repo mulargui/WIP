8da37b49a8a98a4693a1ae43f4a18613e08dae4d
mulargui <mauricio@owlbrite.com>
2018-12-09 17:37:45 -0800

    added masterseed class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,13 @@
 package com.mycompany.app;
 
+import com.mycompany.masterseed;
+
 public class App 
 {
     public static void main( String[] args )throws InterruptedException
     {
+		MasterSeed ms;
+		
 		for(int i=0; i < 10000; i++){
 			System.out.println( "Hello World! Loop: " + i);
 			Thread.sleep(10000);
