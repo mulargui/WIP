e449d17ca45490741de5c44b2a93d633d5b18779
Mauricio Ulargui <mauricio@ulargui.com>
2019-01-31 16:31:05 -0800

    project update

--- k8s/setup.bat
+++ k8s/setup.bat
@@ -1,11 +1,14 @@
 rem you need to run this shellscript as an administrator
 
-rem install minikube
+rem install kubectl
+rem https://kubernetes.io/docs/setup/release/notes/
 for /f %%i in ('curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt') do set kctlver=%%i
 curl -LO https://storage.googleapis.com/kubernetes-release/release/%kctlver%/bin/windows/amd64/kubectl.exe
 move kubectl.exe %systemroot%\system32
 
-set k8ver=v0.30.0
+rem install minikube
+rem https://github.com/kubernetes/minikube/releases
+set k8ver=v0.33.1
 curl -LO https://storage.googleapis.com/minikube/releases/%k8ver%/minikube-windows-amd64.exe
 ren minikube-windows-amd64.exe minikube.exe
 move minikube.exe %systemroot%\system32
@@ -15,7 +18,8 @@ minikube start
 minikube status
 
 rem install helm
-set helmver=v2.12.0
+rem https://github.com/helm/helm/releases
+set helmver=v2.12.3
 curl -LO https://storage.googleapis.com/kubernetes-helm/helm-%helmver%-windows-amd64.zip
 powershell -command "Expand-Archive .\helm-%helmver%-windows-amd64.zip .\helm"
 move .\helm\windows-amd64\helm.exe %systemroot%\system32
