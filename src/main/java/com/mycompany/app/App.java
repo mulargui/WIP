2c445d782d9dd0175abb09d4abe38295d8d1f925
mulargui <mauricio@ulargui.com>
2019-02-02 17:48:43 -0800

    refining setup

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,9 @@
 package com.mycompany.app;
 
-import com.mycompany.app.MasterSeed;
-
 public class App 
 {
     public static void main( String[] args )throws InterruptedException
-    {
-		MasterSeed ms;
-		
+    {	
 		for(int i=0; i < 10000; i++){
 			System.out.println( "Hello World! Loop: " + i);
 			Thread.sleep(10000);
