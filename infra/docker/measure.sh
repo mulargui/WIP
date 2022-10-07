68fc1bde0bcaf24c303d165bcf518a72c5f91c3a
Mauricio Ulargui <ulargm@amazon.com>
2022-10-07 15:29:57 -0700

    Added measurement code

--- infra/docker/measure.sh
+++ infra/docker/measure.sh
@@ -1,3 +1,5 @@
 date +"%T.%3N"
 curl 127.0.0.1 
 date +"%T.%3N"
+curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": "2507", "Aspect": "160",  "Slope": "8" }' http://127.0.0.1/predict
+date +"%T.%3N"
