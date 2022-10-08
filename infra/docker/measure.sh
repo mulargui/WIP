666b0fd4450ee125d59bf68696c8e9a6df2d7cdd
Mauricio Ulargui <ulargm@amazon.com>
2022-10-08 08:14:01 -0700

    changes in calling the model

--- infra/docker/measure.sh
+++ infra/docker/measure.sh
@@ -1,5 +1,5 @@
 date +"%T.%3N"
 curl 127.0.0.1 
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": "2507", "Aspect": "160",  "Slope": "8" }' http://127.0.0.1/predict
+curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 160.0,  "Slope": 8.0 }' http://127.0.0.1/predict/
 date +"%T.%3N"
--- src/api/main.py
+++ src/api/main.py
@@ -77,6 +77,11 @@ async def index():
 @app.post('/predict/')
 async def predict(request: Request):
 
+    print('Elevation: ', request.Elevation)
+    print('Aspect: ', request.Aspect)
+    print('Slope: ', request.Slope)
+    print('Horizontal_Distance_To_Hydrology: ', request.Horizontal_Distance_To_Hydrology)
+
     #there needs to be a better way
     x = tf.convert_to_tensor([
         request.Elevation,
@@ -135,6 +140,62 @@ async def predict(request: Request):
         request.Soil_Type40
         ])
 
-    prediction = model.predict(x)
+    #prediction = model.predict(x)
+    prediction = model.predict([
+        request.Elevation,
+        request.Aspect,
+        request.Slope,
+        request.Horizontal_Distance_To_Hydrology,
+        request.Vertical_Distance_To_Hydrology,
+        request.Horizontal_Distance_To_Roadways,
+        request.Hillshade_9am,
+        request.Hillshade_Noon,
+        request.Hillshade_3pm,
+        request.Horizontal_Distance_To_Fire_Points,
+        request.Wilderness_Area1,
+        request.Wilderness_Area2,
+        request.Wilderness_Area3,
+        request.Wilderness_Area4,
+        request.Soil_Type1,
+        request.Soil_Type2,
+        request.Soil_Type3,
+        request.Soil_Type4,
+        request.Soil_Type5,
+        request.Soil_Type6,
+        request.Soil_Type7,
+        request.Soil_Type8,
+        request.Soil_Type9,
+        request.Soil_Type10,
+        request.Soil_Type11,
+        request.Soil_Type12,
+        request.Soil_Type13,
+        request.Soil_Type14,
+        request.Soil_Type15,
+        request.Soil_Type16,
+        request.Soil_Type17,
+        request.Soil_Type18,
+        request.Soil_Type19,
+        request.Soil_Type20,
+        request.Soil_Type21,
+        request.Soil_Type22,
+        request.Soil_Type23,
+        request.Soil_Type24,
+        request.Soil_Type25,
+        request.Soil_Type26,
+        request.Soil_Type27,
+        request.Soil_Type28,
+        request.Soil_Type29,
+        request.Soil_Type30,
+        request.Soil_Type31,
+        request.Soil_Type32,
+        request.Soil_Type33,
+        request.Soil_Type34,
+        request.Soil_Type35,
+        request.Soil_Type36,
+        request.Soil_Type37,
+        request.Soil_Type38,
+        request.Soil_Type39,
+        request.Soil_Type40
+        ])
 
     return {"prediction": int(prediction)}
