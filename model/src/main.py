b5875a3df924653336efc88868065e40ca0b5746
Mauricio Ulargui <ulargm@amazon.com>
2022-10-11 16:18:42 -0700

    created a model class

--- model/src/main.py
+++ model/src/main.py
@@ -1,13 +1,3 @@
-#constants
-NUM_FEATURES = 54
-NUM_CLASSES = 7
-NUM_EPOCS = 75
-
-#avoid TF INFO messages about internal optimizations (This TensorFlow binary is optimized with ...)
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-
-
 
 from data import Data
 d = Data('/data/train.csv')
@@ -15,21 +5,7 @@ d = Data('/data/train.csv')
 d.data_engineering()
 d.split(0.2)
 
-#here is the NN model
-import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-
-model = Sequential()
-model.add(Dense(units = NUM_FEATURES * 2/3, activation = 'relu', kernel_initializer = 'normal', input_dim = NUM_FEATURES))
-model.add(Dense(units = NUM_CLASSES, activation = 'softmax'))
-model.compile(loss = keras.losses.categorical_crossentropy,
-              optimizer = 'Adam',
-              metrics = ['accuracy'])
-
-#train the model
-model.fit(d.x_train, d.y_train, validation_data = (d.x_test, d.y_test), epochs = NUM_EPOCS)
-
-#save the model
-model.save('/model-registry/model/')
+from model import Model
+m = Model()
+m.train(d.x_train, d.y_train, d.x_test, d.y_test)
+m.save()
---
+++ model/src/model.py
@@ -0,0 +1,34 @@
+#constants
+NUM_FEATURES = 54
+NUM_CLASSES = 7
+
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+
+#avoid TF INFO messages about internal optimizations (This TensorFlow binary is optimized with ...)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+
+class Model:
+    NUM_EPOCS = 75
+
+    #constructor
+    def __init__(self):
+        #here is the NN model
+        self.model = Sequential()
+        self.model.add(Dense(units = NUM_FEATURES * 2/3, activation = 'relu', kernel_initializer = 'normal', input_dim = NUM_FEATURES))
+        self.model.add(Dense(units = NUM_CLASSES, activation = 'softmax'))
+        self.model.compile(loss = keras.losses.categorical_crossentropy,
+            optimizer = 'Adam',
+            metrics = ['accuracy'])
+
+    #train the model
+    def train(self, x_train, y_train, x_test, y_test):
+        self.model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = NUM_EPOCS)
+    
+    #save the model
+    def save(self):
+        self.model.save('/model-registry/model/')
+
