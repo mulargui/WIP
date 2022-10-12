7c59aaa17f57d1c1de0706332e9fdbf177e532e3
Mauricio Ulargui <ulargm@amazon.com>
2022-10-11 17:26:42 -0700

    bug fixing

--- model/src/data.py
+++ model/src/data.py
@@ -9,23 +9,24 @@ class Data:
         #read the csv file in a pandas dataframe
         df = pd.read_csv(source)
 
-        print ('uno')
-        print(df.isna().sum().sum())
-        print(df.isnull().sum().sum())
-        print('dos')
-        print(df[df.drop(['Id','Cover_Type'], axis=1).eq(0).all(1)].empty)
+        #validate the data source
+        data_prevalidation(df)
 
         #split in features and labels
         self.y = df.Cover_Type
         self.x = df.drop(['Cover_Type'], axis=1)
 
     #data validation - pre
-    #def data_prevalidation(self):
-        #validate files - nans per feature
-        #print(df.isnull().sum(axis = 0))
+    def data_prevalidation(self, df):
+        #validate nans per feature
+        print ('uno')
+        print(df.isna().sum().sum())
+        print(df.isnull().sum().sum())
+        print(df.isnull().sum(axis = 0))
 
-        #validate files - no rows with all zeros
-        #print(df[df.drop(['Id','Cover_Type'], axis=1).eq(0).all(1)].empty)
+        #validate no rows with all zeros
+        print('dos')
+        print(df[df.drop(['Id','Cover_Type'], axis=1).eq(0).all(1)].empty)
 
     #data validation - post
     def data_postvalidation(self):
