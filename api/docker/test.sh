0615f773cd627d5bb70ceb316f941df139cfbc08
Mauricio Ulargui <ulargm@amazon.com>
2022-10-12 08:29:44 -0700

    tests

--- api/docker/test.sh
+++ api/docker/test.sh
@@ -1,13 +1,13 @@
 date +"%T.%3N"
 curl -i 127.0.0.1 ; echo
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 160.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
+curl -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 160.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 7.0, "Aspect": 160.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
+curl -X POST -H 'Content-Type: application/json' -d '{"Elevation": 7.0, "Aspect": 160.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 1.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
+curl -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 1.0,  "Slope": 8.0 }' http://127.0.0.1/predict/ ; echo
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 160.0,  "Slope": 80.0 }' http://127.0.0.1/predict/ ; echo
+curl -X POST -H 'Content-Type: application/json' -d '{"Elevation": 2507.0, "Aspect": 160.0,  "Slope": 80.0 }' http://127.0.0.1/predict/ ; echo
 date +"%T.%3N"
-curl -i -X POST -H 'Content-Type: application/json' -d '{"Elevation": 7.0, "Aspect": 1.0,  "Slope": 80.0 }' http://127.0.0.1/predict/ ; echo
+curl -X POST -H 'Content-Type: application/json' -d '{"Elevation": 7.0, "Aspect": 1.0,  "Slope": 80.0 }' http://127.0.0.1/predict/ ; echo
 date +"%T.%3N"
