0189832a4e531b8b95412d70ed4713062466c5c3
Mauricio Ulargui <ulargm@amazon.com>
2022-12-04 13:00:05 -0800

    fixing issues

--- model/src/runlocal.py
+++ model/src/runlocal.py
@@ -7,8 +7,8 @@ from sagemaker.tensorflow import TensorFlow
 
 tf_estimator = TensorFlow(entry_point='mnist_keras_tf.py', 
                           role=role,
-                          train_instance_count=1, 
-                          train_instance_type='local',
+                          instance_count=1, 
+                          instance_type='local',
                           framework_version='1.15', 
                           py_version='py3',
                           script_mode=True,
