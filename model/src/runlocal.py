b4b342da9b0666e3b77cae3688a153d098e2ac3d
Mauricio Ulargui <ulargm@amazon.com>
2022-12-04 15:41:48 -0800

    fixing issues

--- model/src/runlocal.py
+++ model/src/runlocal.py
@@ -6,14 +6,13 @@ role = sagemaker.get_execution_role()
 from sagemaker.tensorflow import TensorFlow
 
 tf_estimator = TensorFlow(entry_point='main.py', 
-                          role=role,
-                          instance_count=1, 
-                          instance_type='local',
-                          framework_version='2.0', 
-                          py_version='py3',
-                          script_mode=True
-                        )
+  role=role,
+  instance_count=1, 
+  instance_type='local',
+  framework_version='2.1.0', 
+  py_version='py3',
+  script_mode=True
+)
 
 local_training_input_path = 'file://data/train.csv'
 tf_estimator.fit(local_training_input_path)
-#tf_estimator.fit()
