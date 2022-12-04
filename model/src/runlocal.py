7c2a92eebd5bc4011c5d5b97681ab4f3d2627365
Mauricio Ulargui <ulargm@amazon.com>
2022-12-04 12:37:16 -0800

    running locally

--- model/src/runlocal.py
+++ model/src/runlocal.py
@@ -1,15 +1,7 @@
-%%sh
-pip -q install sagemaker awscli boto3 tensorflow keras --upgrade 
-
 import sagemaker
 
 sess = sagemaker.Session()
 role = sagemaker.get_execution_role()
-
-# Where to save the model
-%env SM_MODEL_DIR='../../model-registry'
-# Where the dataset is
-%env SM_DATASET_DIR='../../data'
 
 from sagemaker.tensorflow import TensorFlow
 
