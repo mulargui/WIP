24589c9927f372085956879db1272e49eeb67ba9
Mauricio Ulargui <ulargm@amazon.com>
2022-12-12 18:54:02 -0800

    upload to s3

--- model/src/runlocal.py
+++ model/src/runlocal.py
@@ -20,6 +20,22 @@ if __name__ == '__main__':
     hyperparameters={'epochs': 1}
   )
 
+  """
+  tf_estimator = TensorFlow(entry_point='main.py', 
+                          role=role,
+                          train_instance_count=1, 
+                          train_instance_type='ml.p3.2xlarge',
+                          framework_version='2.1.0', 
+                          py_version='py3',
+                          script_mode=True,
+                          train_use_spot_instances=True,        # Use spot instance
+                          train_max_run=600,                    # Max training time
+                          train_max_wait=3600,                  # Max training time + spot waiting time
+                          model_dir='/opt/ml/model',
+                          hyperparameters={'epochs': 1}
+                         )
+  """
+
   #run the job
   local_training_input_path = 'file://data/train.csv'
   tf_estimator.fit({'training': training_input_path})
