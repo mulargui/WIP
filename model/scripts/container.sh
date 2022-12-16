e1aed4f516ae5d8dcae7a078a2d864f0d7dce8c7
Mauricio Ulargui <ulargm@amazon.com>
2022-12-15 19:02:07 -0800

    update

---
+++ model/scripts/container.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set +x
+export DEBIAN_FRONTEND=noninteractive
+# Absolute path to this repo
+SCRIPT=$(readlink -f "$0")
+export REPOPATH=$(dirname "$SCRIPT" | sed 's/\/model\/scripts//g')
+
+# what you can do
+LOCAL=N
+SGLOCAL=N
+SG=N
+
+# you can also set the flags using the command line
+for var in "$@"
+do
+	if [ "LOCAL" == "$var" ]; then LOCAL=Y 
+	fi
+	if [ "SGLOCAL" == "$var" ]; then SGLOCAL=Y 
+	fi
+	if [ "SG" == "$var" ]; then SG=Y 
+	fi
+done
+
+#train locally
+if [ "${LOCAL}" == "Y" ]; then
+	docker run -ti -v $REPOPATH:/WIP tensorflow/tensorflow /bin/bash /WIP/model/scripts/trainlocal.sh
+fi
+
+#train locally using sagemaker
+if [ "${SGLOCAL}" == "Y" ]; then
+	docker run -ti -v $REPOPATH:/WIP tensorflow/tensorflow /bin/bash /WIP/model/scripts/trainsglocal.sh
+fi
+
+#train in sagemaker
+if [ "${SG}" == "Y" ]; then
+	docker run -ti -v $REPOPATH:/WIP tensorflow/tensorflow /bin/bash /WIP/model/scripts/trainmodel.sh
+fi
