1e69c1a2ccac30196b6c834ffd90c8095341d101
Mauricio Ulargui <ulargm@amazon.com>
2022-12-18 17:50:47 -0800

    update

--- api/src/main.py
+++ api/src/main.py
@@ -1,17 +1,9 @@
-import argparse, os
 import sagemaker
 from sagemaker.tensorflow import TensorFlowModel
+from sagemaker.serverless.serverless_inference_config import ServerlessInferenceConfig
 
 if __name__ == '__main__':
-    
-"""
-    #get job arguments from sagemaker env
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    args, _ = parser.parse_known_args()
-    model_dir  = args.model_dir
-"""
-    #sess = sagemaker.Session()
+
     role = sagemaker.get_execution_role()
     model_url = "s3://sagemaker-us-east-1-867679111813/tensorflow-training-2022-12-17-17-26-10-301/output/model.tar.gz"
 
@@ -22,8 +14,5 @@ if __name__ == '__main__':
     )
 
     #endpoint creation
-    model = TensorFlowModel(model_data=model_url , role=role)
+    model = TensorFlowModel(model_data=model_url , role=role,   framework_version='2.1.0')
     model.deploy(serverless_inference_config=serverless_config)
-
-    #remove endpoint
-    #predictor.delete_endpoint()
