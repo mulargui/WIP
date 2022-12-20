e343cbe1440b2d9463d9461207a1673c3bcf6a16
Mauricio Ulargui <ulargm@amazon.com>
2022-12-20 11:31:30 -0800

    update

--- model/src/trainsg.py
+++ model/src/trainsg.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
   #role = sagemaker.get_execution_role()
   #role = 'arn:aws:iam::XXXXXXXXXX:role/service-role/AmazonSageMaker-ExecutionRole-YYYYYYYYYY'
   #role = boto3.client('iam').get_role(RoleName='AmazonSageMaker-ExecutionRole-')['Role']['Arn']
-  role = boto3.client('iam').list_roles(PathPrefix='/service-role/AmazonSageMaker-ExecutionRole-')
+  role = boto3.client('iam').list_roles(PathPrefix='/role/service-role/AmazonSageMaker-ExecutionRole-')
   #['Roles'][0]
   #[0]['Arn']
   print(role)
