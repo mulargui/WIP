df48256f2c3241095624813a90d73a4de2b5d749
Mauricio Ulargui <ulargm@amazon.com>
2022-12-21 15:02:57 -0800

    update

--- api/test/main.py
+++ api/test/main.py
@@ -4,23 +4,22 @@ if __name__ == '__main__':
 
     endpoint = 'predict-forest-type'
     payload = '''{ 
-        "instances": { 
-            "values": [ 2507.0, 160.0, 8.0,
-                0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
-                0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
-                0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ] }}'''
-    payload1 = '''{ 
         "instances": [ 2507.0, 160.0, 8.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     }'''
+    payload1='''2507.0, 160.0, 8.0,
+            0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    '''
     
     runtime = boto3.Session().client('sagemaker-runtime')
   
     # Send request via InvokeEndpoint API
-    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
+    #response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
+    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='text/csv', Body=payload1)
 
     # Unpack response
     result = json.loads(response['Body'].read().decode())
