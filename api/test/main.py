c493f374d539a2a30765ad4625ebe28200d86c60
Mauricio Ulargui <ulargm@amazon.com>
2022-12-21 14:34:37 -0800

    update

--- api/test/main.py
+++ api/test/main.py
@@ -11,20 +11,17 @@ if __name__ == '__main__':
                 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0
     ] }}'''
     payload = '''{ 
-        "input_1": [ 2507.0, 160.0, 8.0,
+        "instances": [ 2507.0, 160.0, 8.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     }'''
     
-    #runtime = boto3.Session().client('sagemaker-runtime')
-    runtime = boto3.Session().client('sagemaker')
+    runtime = boto3.Session().client('sagemaker-runtime')
   
     # Send request via InvokeEndpoint API
-    #response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
-    response = runtime.describe_endpoint(EndpointName=endpoint)
-    print (response)
+    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
 
     # Unpack response
-    #result = json.loads(response['Body'].read().decode())
-    #print(result)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
