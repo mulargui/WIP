8f441ab8e8ed38d66ffab0cd5edf8725b7cdc960
Mauricio Ulargui <ulargm@amazon.com>
2023-03-14 10:59:44 -0700

    scripts

--- docker-healthylinkx-cli.sh
+++ docker-healthylinkx-cli.sh
@@ -1,5 +1,4 @@
 #similar to healthylinkx-cli.sh but running inside a container to avoid to insltall node, npm...
 docker run -ti --rm -v $PWD:/healthylinkx-lex -w /healthylinkx-lex/ \
 	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ACCOUNT_ID \
-	-e AWS_REGION -e AWS_DEFAULT_REGION node /bin/bash
-	#healthylinkx-cli.sh "$@"
+	-e AWS_REGION -e AWS_DEFAULT_REGION node /bin/bash healthylinkx-cli.sh "$@"
