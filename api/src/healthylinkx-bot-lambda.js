6f3b5a3681047c1613373b1f4a890b20afe179df
Mauricio Ulargui <ulargm@amazon.com>
2023-03-13 18:05:29 -0700

    lambda

--- api/src/healthylinkx-bot-lambda.js
+++ api/src/healthylinkx-bot-lambda.js
@@ -20,8 +20,9 @@ function ServerReply (code, event){
         messages: [{
             contentType: 'PlainText',
             //content: 'original event: ' + JSON.stringify(event) + '#end'
-            content: 'intent: ' +  event.interpretations[0].intent.name +
-                ' value: ' + event.interpretations[0].intent.slots."DoctorName".value.interpretedValue + '#end'
+            content: 'intent: ' +  JSON.stringify(event.interpretations[0])
+            //content: 'intent: ' +  event.interpretations[0].intent.name 
+                //+ ' value: ' + event.interpretations[0].intent.slots."DoctorName".value.interpretedValue + '#end'
         }],
         sessionId: event.sessionId,
         requestAttributes: event.requestAttributes
