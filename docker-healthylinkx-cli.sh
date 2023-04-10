6ffb65972016909ff5108d96188b531d1695f45c
Mauricio Ulargui <ulargm@amazon.com>
2023-04-10 16:20:51 -0700

    credentials

--- docker-healthylinkx-cli.sh
+++ docker-healthylinkx-cli.sh
@@ -3,7 +3,13 @@ if [ "$(docker images | grep node-ask-cli)" == "" ]; then
 	docker build --rm=true -t node-ask-cli $PWD/docker
 fi
 
+#check we have .ask directory
+if [ ! -d $HOME/.ask ]; then
+	echo ".ask directory doesn't exist, creating it"
+	mkdir $HOME/.ask
+fi
+
 #similar to healthylinkx-cli.sh but running inside a container to avoid to install node, npm...
-docker run -ti --rm -v $PWD:/healthylinkx-ask -v $HOME/.ask:/.ask \
+docker run -ti --rm -v $PWD:/healthylinkx-ask -v $HOME/.ask:/root/.ask \
 	-w /healthylinkx-ask/ \
 	node-ask-cli /bin/bash
