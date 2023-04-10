af20bdce104ed1a5bfc7471ae53b32b5763440c8
Mauricio Ulargui <ulargm@amazon.com>
2023-04-10 16:02:06 -0700

    including credentials

--- docker-healthylinkx-cli.sh
+++ docker-healthylinkx-cli.sh
@@ -4,9 +4,6 @@ if [ "$(docker images | grep node-ask-cli)" == "" ]; then
 fi
 
 #similar to healthylinkx-cli.sh but running inside a container to avoid to install node, npm...
-#docker run -ti --rm -v $PWD:/healthylinkx-lex -w /healthylinkx-lex/ \
-#	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ACCOUNT_ID \
-#	-e AWS_REGION -e AWS_DEFAULT_REGION node-ask-cli /bin/bash healthylinkx-cli.sh "$@"
-
-docker run -ti --rm -v $PWD:/healthylinkx-ask -w /healthylinkx-ask/ \
+docker run -ti --rm -v $PWD:/healthylinkx-ask -v $HOME/.ask:/.ask \
+	-w /healthylinkx-ask/ \
 	node-ask-cli /bin/bash
