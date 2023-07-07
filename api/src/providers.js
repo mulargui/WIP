f3bcfb551cb447f220d170f19778348c8421d0f1
Mauricio Ulargui <ulargm@amazon.com>
2023-07-07 14:34:48 -0700

    search lambda

--- api/src/providers.js
+++ api/src/providers.js
@@ -1,9 +1,9 @@
-//const constants = require("./constants.js");
-//const mysql = require('mysql2/promise');
+const constants = require("./constants.js");
+const mysql = require('mysql2/promise');
 
 function SearchDoctors(DoctorName, ZipCode, Gender)
 {	
-	var speakOutput = `You just triggered a search for doctors with the following slots: `;
+	var speakOutput = `You just triggered a search for doctors with the following value slots: `;
 	speakOutput += Gender ? `${Gender} ` : ``;
 	speakOutput += ZipCode ? `${ZipCode} ` : ``;
 	speakOutput += DoctorName ? `${DoctorName} ` : ``;
