b931294d974843f7e5adf832a9fb4a17e156979f
Mauricio Ulargui <ulargm@amazon.com>
2023-07-06 17:55:55 -0700

    preparing lambda

--- healthylinkx-cli.sh
+++ healthylinkx-cli.sh
@@ -7,7 +7,7 @@ node infra/src/index.ts "$@"
 #check we have ASK credentials
 if [ ! -e $HOME/.ask/cli_config ]; then
 	echo "No ASK credentials, requesting them"
-	ask configure --no-browser
+	#ask configure --no-browser
 fi
 
 #create a new skill
