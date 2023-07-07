65264d418d054abb9170c16b8b4544528a86491d
Mauricio Ulargui <ulargm@amazon.com>
2023-07-07 15:23:22 -0700

    adding the datastore slowly

--- api/src/providers.js
+++ api/src/providers.js
@@ -1,8 +1,41 @@
 const constants = require("./constants.js");
 const mysql = require('mysql2/promise');
 
+var db = mysql.createPool({
+	host:constants.host,
+	user:constants.user,
+	password:constants.password,
+	database:constants.database
+});
+
+function FormatResult(rows){
+    if (rows == null) return 'Sorry, no matching providers were found.';
+    if (!rows.length) return 'Sorry, no matching providers were found.';
+	
+    var output = `These are the doctors we found for you: `;
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].hasOwnProperty(0)) 
+            output += ("Name: " + rows[i][0]);
+		if (rows[i].hasOwnProperty(1)) 
+            output += ("Address: " + rows[i][1]);
+		if (rows[i].hasOwnProperty(2)) 
+            output += ("City: " + rows[i][2]);
+    }
+    return output;
+}
+
 function SearchDoctors(DoctorName, ZipCode, Gender)
 {	
+	//check params
+ 	if(!ZipCode && !DoctorName && !Gender)
+		return "Sorry, I need more information to  search for doctors";
+
+	//normalize gender
+	if (Gender){
+		if (Gender === 'male') Gender = 'M';
+		if (Gender === 'm') Gender = 'M';
+		if (Gender !== 'M') Gender = 'F';
+	}
 	var speakOutput = `You just triggered a search for doctors with the following values:`;
 	speakOutput += Gender ? ` gender = ${Gender}` : ``;
 	speakOutput += ZipCode ? ` zipcode = ${ZipCode}` : ``;
