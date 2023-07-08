be99ff8afbea357de379ea3e40b5f04b99654c29
Mauricio Ulargui <ulargm@amazon.com>
2023-07-08 10:39:41 -0700

    debugging

--- api/src/providers.js
+++ api/src/providers.js
@@ -76,7 +76,7 @@ async function SearchDoctors(DoctorName, ZipCode, Gender)
 		db.on('error', function (err) {
 			  return 'pool error  ${err}`;
 			});*/
-		//await db.query(query);
+		await db.query(query);
 		ret = query;
 		//var ret = await sleepwrapper(1);
 		//return ret;
@@ -84,9 +84,7 @@ async function SearchDoctors(DoctorName, ZipCode, Gender)
 		//return FormatResult(rows);
    	} catch(err) {
 	   	ret = 'Error in query';
-   	} finally {
-		ret = 'finally block';
-	}
+   	} 
 
 	return ret;
 } 
