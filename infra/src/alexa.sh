fe98db14a432f40b79cb043175890a534ce86aaf
Mauricio Ulargui <ulargm@amazon.com>
2023-07-15 18:33:17 -0700

    alexa skill

--- infra/src/alexa.sh
+++ infra/src/alexa.sh
@@ -4,5 +4,5 @@
 
 #ask smapi export-package --skill-id amzn1.ask.skill.34e99e55-f047-4ef0-9708-92d6bb4d66ab --stage development
 
-#ask smapi import-skill-package --location . --skill-id amzn1.ask.skill.34e99e55-f047-4ef0-9708-92d6bb4d66ab 
+#ask smapi import-skill-package --location https://github.com/mulargui/WIP/tree/master/alexa/src --skill-id amzn1.ask.skill.34e99e55-f047-4ef0-9708-92d6bb4d66ab 
 
