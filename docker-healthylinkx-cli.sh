bf5fdea3114881df908af8a09966af17e2d08798
Mauricio Ulargui <ulargm@amazon.com>
2023-07-22 17:54:46 -0700

    ux infra

--- docker-healthylinkx-cli.sh
+++ docker-healthylinkx-cli.sh
@@ -4,7 +4,7 @@ if [ "$(docker images | grep node-ask-cli)" == "" ]; then
 fi
 
 #similar to healthylinkx-cli.sh but running inside a container to avoid to install node, npm...
-docker run -ti --rm -v $PWD:/repo -v $HOME/alexa:/healthylinkx-ask -v $HOME/.ask:/root/.ask \
+docker run -ti --rm -v $PWD:/repo -v $HOME/.ask:/root/.ask \
 	-w /repo/ \
 	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ACCOUNT_ID \
 	-e AWS_REGION -e AWS_DEFAULT_REGION \
