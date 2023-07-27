378ad213d35c29ed26592cc4eb16c75592cd7aca
Mauricio Ulargui <ulargm@amazon.com>
2023-07-26 18:11:22 -0700

    cleanup

--- infra/src/alexa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#ask configure --profile <profile> --no-browser
-
-#ask smapi export-package --skill-id amzn1.ask.skill.34e99e55-f047-4ef0-9708-92d6bb4d66ab --stage development
-
-#ask smapi import-skill-package --location https://github.com/mulargui/WIP/tree/master/alexa/src --skill-id amzn1.ask.skill.34e99e55-f047-4ef0-9708-92d6bb4d66ab 
-
