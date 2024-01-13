27d787b249a264f3d098a1ac141aa7b337005664
Mauricio Ulargui <ulargm@amazon.com>
2024-01-12 20:03:40 -0800

    crreating image

--- infra/run.sh
+++ infra/run.sh
@@ -11,22 +11,18 @@ export DEBIAN_FRONTEND=noninteractive
 # Absolute path to this repo
 SCRIPT=$(readlink -f "$0")
 export REPOPATH=$(dirname "$SCRIPT" | sed 's/\/infra//')
-#export REPOPATH=$(dirname "$SCRIPT")
-
-echo $SCRIPT
-echo $REPOPATH
-echo $PWD
-exit
 
 # check if the image is already built, if not build it
 if [ "$(docker images | grep base_image)" == "" ]; then
-	docker build --rm=true -t base_image $PWD/docker
+	docker build --rm=true -t base_image $REPOPATH/docker
 fi
 
+exit 
+
 # run the query
-docker run -ti --rm -v $PWD:/repo \
+docker run -ti --rm -v $REPOPATH:/repo \
 	-w /repo/ \
 	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ACCOUNT_ID \
 	-e AWS_REGION -e AWS_DEFAULT_REGION \
-	base_image python3 /repo/healthylinkx-cli.sh "$@"
-	#base_image /bin/bash /repo/healthylinkx-cli.sh "$@"
+	base_image python3 /repo/src/prompt.py "$@"
+	#base_image /bin/bash
