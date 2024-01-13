ac32374d5ae1a1b62774c9d683c95b1253c8daea
Mauricio Ulargui <ulargm@amazon.com>
2024-01-12 21:49:01 -0800

    modules!

---
+++ src/base_ai.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+
+session = boto3.session.Session()
+bedrock_runtime = boto3.client(
+    service_name="bedrock-runtime",
+    region_name=session.region_name,
+)
+
+"""
+Function to run inference with models hosted in Bedrock
+"""
+def run_inference(prompt:str, 
+    model:str="amazon.titan-text-express-v1", 
+    temperature:float=0.0, 
+    max_tokens:int=1000,
+    topP:float=1.0) :
+
+    body = json.dumps({"inputText":prompt,
+        "textGenerationConfig": {
+            "temperature": temperature,
+            "maxTokenCount": max_tokens,
+            "topP": topP
+        }})
+
+    output = bedrock_runtime.invoke_model(body = body, 
+        modelId = model, 
+        accept = 'application/json',
+        contentType = 'application/json')
+ 
+    return json.loads(output.get("body").read()).get('results')[0].get('outputText')
